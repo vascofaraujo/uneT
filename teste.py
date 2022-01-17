c16ac40866d6d2c1c2ef243f15548cb596228ba0
vascofaraujo <vascofaraujo98@pm.me>
2022-01-17 15:13:34 +0000

    unet working nice

--- teste.py
+++ teste.py
@@ -6,6 +6,8 @@ import os
 import nibabel as nib
 import cv2 as cv
 import matplotlib.pyplot as plt
+import random
+from tqdm import tqdm
 
 class BraTSDataset(Dataset):
     def __init__(self):
@@ -21,13 +23,27 @@ class BraTSDataset(Dataset):
         level = self.levels[3] #t1ce
         image = nib.load(self.dataset_folder + self.dataset_list[folder_index] + '/' + self.dataset_list[folder_index] + '_' + level + '.nii.gz').get_fdata()
         seg = nib.load(self.dataset_folder + self.dataset_list[folder_index] + '/' + self.dataset_list[folder_index] + '_seg' + '.nii.gz').get_fdata()
-        image = cv.resize(image[:,:,image.shape[2]//2], (128,128), interpolation=cv.INTER_LINEAR)
+
+        image = image[:,:,image.shape[2]//2]
         image = (image * 255) / np.max(image)
 
-        seg = cv.resize(seg[:,:,seg.shape[2]//2], (128,128), interpolation=cv.INTER_LINEAR)
+        seg = seg[:,:,seg.shape[2]//2]
 
-        image = torch.from_numpy(image)
-        seg = torch.from_numpy(seg)
+        image_pad = np.zeros((256, 256))
+        seg_pad = np.zeros((256, 256))
+
+        image_pad[7:247, 7:247] = image
+        seg_pad[7:247, 7:247] = seg
+
+        n = random.randint(0, 191)
+
+        image_pad = image_pad[n:n+64, n:n+64]
+        seg_pad = seg_pad[n:n+64, n:n+64]
+
+
+        image = torch.from_numpy(image_pad)
+        seg = torch.from_numpy(seg_pad)
+
 
         return image[None,:,:], seg[None,:,:]
 
@@ -36,10 +52,13 @@ class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=5, padding=2):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
         self.relu  = nn.ReLU()
 
     def forward(self, x):
         x = self.conv(x)
+        x = self.relu(x)
+        x = self.conv2(x)
         x = self.relu(x)
 
         return x
@@ -48,13 +67,17 @@ class DecoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, padding):
         super().__init__()
         self.transposed_conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.transposed_conv2 = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.transposed_conv(x)
         x = self.relu(x)
+        x = self.transposed_conv2(x)
+        x = self.relu(x)
 
         return x
+
 
 class UneT(nn.Module):
     def __init__(self):
@@ -64,16 +87,17 @@ class UneT(nn.Module):
         self.encoder3 = EncoderBlock(in_channels=32, out_channels=64)
         self.encoder4 = EncoderBlock(in_channels=64, out_channels=128)
         self.encoder5 = EncoderBlock(in_channels=128, out_channels=256)
+
         self.encoder6 = EncoderBlock(in_channels=256, out_channels=128)
         self.encoder7 = EncoderBlock(in_channels=128, out_channels=64)
         self.encoder8 = EncoderBlock(in_channels=64, out_channels=32)
         self.encoder9 = EncoderBlock(in_channels=32, out_channels=16)
         self.encoder10 = EncoderBlock(in_channels=16, out_channels=4)
 
-        self.decoder1 = DecoderBlock(in_channels=256, out_channels=128, kernel_size=9, padding=0)
-        self.decoder2 = DecoderBlock(in_channels=128, out_channels=64, kernel_size=17, padding=0)
-        self.decoder3 = DecoderBlock(in_channels=64, out_channels=32, kernel_size=33, padding=0)
-        self.decoder4 = DecoderBlock(in_channels=32, out_channels=16, kernel_size=65, padding=0)
+        self.decoder1 = DecoderBlock(in_channels=256, out_channels=128, kernel_size=3, padding=0)
+        self.decoder2 = DecoderBlock(in_channels=128, out_channels=64, kernel_size=5, padding=0)
+        self.decoder3 = DecoderBlock(in_channels=64, out_channels=32, kernel_size=9, padding=0)
+        self.decoder4 = DecoderBlock(in_channels=32, out_channels=16, kernel_size=17, padding=0)
 
         self.pool = nn.MaxPool2d(2)
 
@@ -91,7 +115,6 @@ class UneT(nn.Module):
         p4 = self.pool(c4)
 
         c5 = self.encoder5(p4)
-
         u6 = self.decoder1(c5)
         p6 = torch.cat((u6, c4), 1)
 
@@ -110,44 +133,74 @@ class UneT(nn.Module):
         c9 = self.encoder9(p9)
         y = self.encoder10(c9)
 
-        print(y.shape)
         return torch.softmax(y, dim=1)
 
 def one_hot_encodding(img, ncols=4):
-    print(img.shape)
     out = torch.zeros(img.shape[0], ncols, img.shape[2], img.shape[3])
-    # out[torch.arange(img.size, im] = 1
+
+    out[:,0,:,:] = torch.where(img[:,0,:,:] == 1.0, 1.0, 0.0)
+    out[:,1,:,:] = torch.where(img[:,0,:,:] == 2.0, 1.0, 0.0)
+    out[:,2,:,:] = torch.where(img[:,0,:,:] == 3.0, 1.0, 0.0)
+    out[:,3,:,:] = torch.where(img[:,0,:,:] == 4.0, 1.0, 0.0)
+
     return out
 
 def train_model(model, train_dataloader):
     criterion = nn.BCELoss()
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    for img, seg in train_dataloader:
-        #break
-        #print(img)
-        optimizer.zero_grad()
-        label = one_hot_encodding(seg)
-        output = model(img.float())
-        plt.imshow(output[0,0,:,:].detach().numpy())
-        loss = criterion(output.float(), label)
-        print(loss.item())
-        loss.backward()
-        optimizer.step()
+    epochs = 20
+
+    epoch_loss = []
+    for epoch in tqdm(range(epochs)):
+        train_loss = []
+
+        for img, seg in train_dataloader:
+            optimizer.zero_grad()
+
+            label = one_hot_encodding(seg)
+
+            output = model(img.float())
+
+            print(output.shape, label.shape)
+            loss = criterion(output.float(), label)
+
+            output = output.detach().numpy()
+
+            loss.backward()
+            optimizer.step()
+            # fig = plt.figure(figsize=(8, 8))
+            # fig.add_subplot(2, 4, 1)
+            # plt.imshow(output[0,0,:,:])
+            # fig.add_subplot(2, 4, 2)
+            # plt.imshow(output[0,1,:,:])
+            # fig.add_subplot(2, 4, 3)
+            # plt.imshow(output[0,2,:,:])
+            # fig.add_subplot(2, 4, 4)
+            # plt.imshow(output[0,3,:,:])
+            # fig.add_subplot(2, 4, 5)
+            # plt.imshow(label[0,0,:,:])
+            # fig.add_subplot(2, 4, 6)
+            # plt.imshow(label[0,1,:,:])
+            # fig.add_subplot(2, 4, 7)
+            # plt.imshow(label[0,2,:,:])
+            # fig.add_subplot(2, 4, 8)
+            # plt.imshow(label[0,3,:,:])
+            # plt.show()
+
+            print(loss.item())
+
+            train_loss.append(loss.item())
+        curr_epoch_loss = np.mean(train_loss)
+        print(f"Epoch loss: {curr_epoch_loss}")
 
 
-    fig = plt.figure(figsize=(8, 8))
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(img[0,0,:,:].T, cmap='Greys_r')
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(seg[0,0,:,:].T, cmap='Greys_r')
-    plt.show()
 
 if __name__ == '__main__':
 
     train_dataset = BraTSDataset()
-    train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
     model = UneT()
 
